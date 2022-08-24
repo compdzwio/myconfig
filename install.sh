75706fdf462e7ad16781cedfe97763c3372caafb
dzw <dzw@bogon.io>
2022-08-24 17:55:43 +0800

    del LC_ALL=C.UTF-8

--- install.sh
+++ install.sh
@@ -993,7 +993,7 @@ cp .Xresources "$HOME/"
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 . "$HOME/.cargo/env"
-export LC_ALL="C.UTF-8"
+
 cargo install alacritty
 # cargo install starship --locked
 curl -sS https://starship.rs/install.sh | sh
